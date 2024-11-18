cb2e00cd3d51a46471abda92f8f656c589ae1227
ArtemiiKantserov <artemiikantserov@gmail.com>
2024-11-18 23:33:10 +0300

    bpsk-modulation

--- modules/coder.py
+++ modules/coder.py
@@ -12,7 +12,7 @@ def encode(s: str, hadamard_vector, user_num: int) -> list[int]:
   hadamard_size = int(np.log2(user_num) + 1)
   for i in range(len(s)):
     bits = to_binary(ord(s[i]))
-    ans[i] =  [_ * hadamard_vector for _ in bits]
+    ans[i] =  [2 * _ * hadamard_vector - [1,1] for _ in bits]
     ans[i] = np.concatenate(ans[i], axis=None)
   return ans
 
