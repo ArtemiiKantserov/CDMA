4f24afdb7d052c4c3532323db3a542bcf2bb03f0
t33nsy <krayoshikage2@gmail.com>
2024-11-18 22:19:49 +0300

    Encode done

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("CDMA")
+from modules import coder
+from modules import hadamard
+
+hadamard_matrix = hadamard.hadamard(1)
+
+print(coder.encode("hello", hadamard_matrix[0]))
---
+++ modules/coder.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def to_binary(i : int) -> list[int]:
+  ans = []
+  while i > 0:
+    ans.append(i % 2)
+    i //= 2
+  return ans[::-1]
+
+def encode(s: str, hadamard_vector, user_num: int) -> list[int]:
+  ans = [[0]]*len(s)
+  hadamard_size = int(np.log2(user_num) + 1)
+  for i in range(len(s)):
+    bits = to_binary(ord(s[i]))
+    ans[i] =  [_ * hadamard_vector for _ in bits]
+  return ans
+
