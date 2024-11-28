7f4232355e07eea6d29a67d5a630db5e909b6e9f
ArtemiiKantserov <artemiikantserov@gmail.com>
2024-11-28 21:58:55 +0300

    roblox

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
 import socket
+import threading
+from modules import coder
+from modules import BPSK_modulation
+
+def create_hadamard(s : str):
+    s = s.strip('[]')
+    vec = [int(i) for i in s.split()]
+    n = len(vec) // 4
+    hadamard = []
+    for i in range(0, n * 4, n):
+        hadamard.append([vec[i:i+n]])
+        return hadamard
+def mess_server(client_socket, size = 1024):
+    while True:
+        response = client_socket.recv(size)
+        print(f"{response.decode('utf-8')}")
 
 # Настройки клиента
 HOST = '26.81.95.59'  # IP сервера
 PORT = 12345        # Порт сервера
-
 inp = input("Введите IP и порт для подключения в виде n.n.n.n port: ").split()
 HOST = inp[0]
 PORT = int(inp[1])
@@ -12,15 +27,35 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
     print("[INFO] Подключен к серверу")
     response = client_socket.recv(1024)
-    print(f"[ОТВЕТ СЕРВЕРА] {response.decode('utf-8')}")
-
+    s = response.decode('utf-8')
+    hadamard = create_hadamard(s)
+    input_sever = threading.Thread(
+        target=mess_server, args=(client_socket, 1024)
+    )
+    input_sever.start()
+    response.decode('utf-8')
     while True:
         # Отправка сообщения
         message = input("Введите сообщение (или 'exit' для выхода): ")
         if message.lower() == 'exit':
             break
-        client_socket.send(message.encode('utf-8'))
-
+        strings = []
+        for j in range(len(message) // 1024 + 1):
+            strings.append(message[1024*j:1024*(j+1)])
+        print(strings)
+        strings[-1] += '/0' * (1024 - len(strings[-1]))
+        encoded = [coder.packet_encode(i, hadamard) for i in strings]
+        bpsk_s = []
+        print(encoded[0][0])
+        for i in encoded:
+            for j in i:
+                bpsk_i, _ = BPSK_modulation.bpsk_modulation(j, 100)
+                bpsk_s.append(bpsk_i)
+        demodulated = BPSK_modulation.bpsk_demodulation(bpsk_s, 100)
+        decoded = coder.packet_decode(demodulated, hadamard)
+        #хотели разбить на потоки, чтобы каждый пакет по 1024 байта преобразовывался и отправлялся независимо(для ускорения процесса), но проблемы с увеличением веса пакета из-за bpsk
+        # for i in bpsk_s:
+            # client_socket.sendall(i.encode('utf-8'))
         # Прием ответа от сервера
-        response = client_socket.recv(1024)
-        print(f"[ОТВЕТ СЕРВЕРА] {response.decode('utf-8')}")
+        # response = client_socket.recv(1024)
+        # print(f"[ПОЛУЧЕНО СООБЩЕНИЕ] {response.decode('utf-8')}")
--- modules/coder.py
+++ modules/coder.py
@@ -1,13 +1,8 @@
 import numpy as np
 
 def to_binary(i : int) -> np.array:
-  ans = [-1]*8
-  j = 0
-  while i > 0:
-    ans[j] = (1 if i % 2 > 0 else -1)
-    j += 1
-    i //= 2
-  return ans[::-1]
+  b = bin(i)[2:]
+  return [2*int(j) - 1 for j in '0'*(8-len(b)) + b]
 
 def binary_to_char(bits: np.array):
   ans = 0
@@ -15,6 +10,12 @@ def binary_to_char(bits: np.array):
   for i in range(len(bits)):
     ans += bits[i] * 2**i
   return chr(ans)
+
+def packet_encode(s : str, hadamard_vector: np.array):
+  encoded = []
+  for i in s:
+    encoded.extend(encode(i, hadamard_vector))
+  return encoded 
 
 def encode(s: str, hadamard_vector: np.array) -> np.ndarray:
   ans = [[0]*len(hadamard_vector)]*len(s)
@@ -26,9 +27,18 @@ def encode(s: str, hadamard_vector: np.array) -> np.ndarray:
     ans[i] = np.concatenate(ans[i], axis=None)
   return ans
 
+def packet_decode(encoded, hadamard):
+  decoded = ''
+  for i in encoded:
+    decoded += decode(i, hadamard)
+  return decoded 
+
 def decode(code: np.ndarray, hadamard_vector: np.array) -> str:
   matrix = [code[i:i+len(hadamard_vector)] for i in range(0, len(code), len(hadamard_vector))]
   multiplied = np.matmul(matrix, hadamard_vector)
   ans = [1 if i > 0 else 0 for i in multiplied]
   return binary_to_char(ans)
   
+s = encode("a", [[1, 1], [1, -1]])
+# print(s)
+print(decode(s,  [[1, 1], [1, -1]]))
